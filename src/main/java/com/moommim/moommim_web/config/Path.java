2ea0a40fb3f8fadaaa7bd205919e4b3b6df609b8
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-16 12:46:37 +0700

    Add path constant

---
+++ src/main/java/com/moommim/moommim_web/config/Path.java
@@ -0,0 +1,11 @@
+package com.moommim.moommim_web.config;
+
+public class Path {
+
+    public static final String BASE_PAGE_PATH = "/pages";
+    
+    public static final String BASE_HOME_PAGE_PATH = "/home";
+    
+    public static final String INDEX_VIEW = BASE_PAGE_PATH + BASE_HOME_PAGE_PATH + "/index.jsp";
+    
+}
