1b86f3ec251dae8c8554b795f394c34eb518e722
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-16 12:52:28 +0700

    Change variable

--- src/main/java/com/moommim/moommim_web/config/Path.java
+++ src/main/java/com/moommim/moommim_web/config/Path.java
@@ -2,10 +2,10 @@ package com.moommim.moommim_web.config;
 
 public class Path {
 
-    public static final String BASE_PAGE_PATH = "/pages";
+    public static final String BASE_PAGE = "/pages";
     
-    public static final String BASE_HOME_PAGE_PATH = "/home";
+    public static final String BASE_HOME_PAGE = "/home";
     
-    public static final String INDEX_VIEW = BASE_PAGE_PATH + BASE_HOME_PAGE_PATH + "/index.jsp";
+    public static final String INDEX_VIEW = BASE_PAGE + BASE_HOME_PAGE + "/index.jsp";
     
 }
