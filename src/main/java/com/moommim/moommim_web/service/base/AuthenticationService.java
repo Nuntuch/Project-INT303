76cd4f9ad18dd69dcb5ba32f451fc5a7c0edb342
Unknown <nuntuch13589@gmail.com>
2018-11-17 16:29:00 +0700

    สร้าง AuthenticationService Interface

---
+++ src/main/java/com/moommim/moommim_web/service/base/AuthenticationService.java
@@ -0,0 +1,16 @@
+package com.moommim.moommim_web.service.base;
+
+import com.moommim.moommim_web.model.UserAccount;
+import javax.servlet.http.HttpSession;
+
+public interface AuthenticationService {
+
+    UserAccount login(String username, String password);
+    
+    boolean logout(HttpSession session); 
+    
+    boolean isLogin();
+    
+    
+    
+}
