91ff9df09a0807d1088e9e9e90a4ea7565a0f745
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-17 16:14:24 +0700

    Generate jpa controller from model

---
+++ src/main/java/com/moommim/moommim_web/repository/AdsJpaController.java
@@ -0,0 +1,153 @@
+
+package com.moommim.moommim_web.repository;
+
+import com.moommim.moommim_web.model.Ads;
+import com.moommim.moommim_web.repository.exceptions.NonexistentEntityException;
+import com.moommim.moommim_web.repository.exceptions.RollbackFailureException;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+
+public class AdsJpaController implements Serializable {
+
+    public AdsJpaController(UserTransaction utx, EntityManagerFactory emf) {
+        this.utx = utx;
+        this.emf = emf;
+    }
+    private UserTransaction utx = null;
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Ads ads) throws RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            em.persist(ads);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Ads ads) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            ads = em.merge(ads);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = ads.getId();
+                if (findAds(id) == null) {
+                    throw new NonexistentEntityException("The ads with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            Ads ads;
+            try {
+                ads = em.getReference(Ads.class, id);
+                ads.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The ads with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(ads);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Ads> findAdsEntities() {
+        return findAdsEntities(true, -1, -1);
+    }
+
+    public List<Ads> findAdsEntities(int maxResults, int firstResult) {
+        return findAdsEntities(false, maxResults, firstResult);
+    }
+
+    private List<Ads> findAdsEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Ads.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Ads findAds(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Ads.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getAdsCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Ads> rt = cq.from(Ads.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+
+}
