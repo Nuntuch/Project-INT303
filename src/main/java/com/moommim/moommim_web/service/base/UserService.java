028f4c21f8a68b668ce833289b25cb9c99b87e60
Unknown <pattariya.th@mail.kmutt.ac.th>
2018-11-17 17:41:46 +0700

    สร้าง method ใน user interface

--- src/main/java/com/moommim/moommim_web/service/base/UserService.java
+++ src/main/java/com/moommim/moommim_web/service/base/UserService.java
@@ -1,11 +1,15 @@
 package com.moommim.moommim_web.service.base;
 
-/**
- *
- * 
- */
-
+import com.moommim.moommim_web.model.UserAccount;
+import java.util.List;
 
 public interface UserService {
-    
+
+    boolean createUser(UserAccount userAccount);
+    boolean editUser(UserAccount editAccount);
+    boolean deleteUser(UserAccount deleteAccount);
+    UserAccount getUserById(int userId);
+    List<UserAccount> getAllUser();
+    boolean isActivate(String isActivateUser);
+    boolean activeUser(String activeUser);
 }
