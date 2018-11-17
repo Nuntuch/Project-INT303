0eeba1e6984b0e3531b1a8429f7f7d56a20f60a5
Unknown <pattariya.th@mail.kmutt.ac.th>
2018-11-17 14:11:41 +0700

    สร้าง Interface ของ UserService

---
+++ src/main/java/com/moommim/moommim_web/service/base/UserService.java
@@ -0,0 +1,12 @@
+ Error reading included file Templates/Classes/Templates/Licenses/license-none.txt
+package com.moommim.moommim_web.service.base;
+
+/**
+ *
+ * @author Ptry
+ */
+
+
+public interface UserService {
+    
+}
