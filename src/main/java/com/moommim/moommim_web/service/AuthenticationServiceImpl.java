86593dd04d6361a2618bd17649b0ed5e4a310205
Unknown <nuntuch13589@gmail.com>
2018-11-18 13:46:44 +0700

    เพิ่ม inner class สำหรับส่งเมล์ รอ Libery

--- src/main/java/com/moommim/moommim_web/service/AuthenticationServiceImpl.java
+++ src/main/java/com/moommim/moommim_web/service/AuthenticationServiceImpl.java
@@ -3,12 +3,15 @@ package com.moommim.moommim_web.service;
 import com.moommim.moommim_web.model.UserAccount;
 import com.moommim.moommim_web.model.controller.UserAccountJpaController;
 import com.moommim.moommim_web.service.base.AuthenticationService;
+import java.net.PasswordAuthentication;
 import java.util.List;
+import java.util.Properties;
 import javax.annotation.Resource;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.servlet.http.HttpSession;
 import javax.transaction.UserTransaction;
+import javax.websocket.Session;
 
 /**
  *
@@ -21,8 +24,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Resource
     UserTransaction utx;
-    
-   private UserAccountJpaController accountJpaController = new UserAccountJpaController(utx, emf);
+
+    private UserAccountJpaController accountJpaController = new UserAccountJpaController(utx, emf);
 
     @Override
     public UserAccount login(String username, String password) {
@@ -80,6 +83,44 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
                     try {
                         //คำสั่งส่งEmail
+                        class SendMail {
+
+                            public int send(String to, String sub,
+                                    String msg) {
+                                String username = "test@gmail.com";
+                                String password = "555";
+
+                                Properties props = new Properties();
+
+                                props.put("mail.smtp.host", "smtp.gmail.com");
+                                //below mentioned mail.smtp.port is optional
+                                props.put("mail.smtp.port", "587");
+                                props.put("mail.smtp.auth", "true");
+                                props.put("mail.smtp.starttls.enable", "true");
+
+                                Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+                                    protected PasswordAuthentication getPasswordAuthentication() {
+                                        return new PasswordAuthentication(username, password);
+                                    }
+                                });
+
+                                try {
+
+                                    MimeMessage message = new MimeMessage(session);
+                                    message.setFrom(new InternetAddress(username, "Heh-Maybe-You-Forgot-To-Change-Header"));
+                                    message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+                                    message.setSubject(sub);
+                                    message.setContent(msg, "text/html; charset=utf-8");
+
+                                    Transport.send(message);
+                                    return 0;
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    return -1;
+                                }
+                            }
+                        }
+
                         return true;
                     } catch (Exception e) {
                         return false;
@@ -88,6 +129,28 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 }
             }
         }
+
+        class EmailMsgManager {
+
+            private String msg;
+
+            public EmailMsgManager() {
+            }
+
+            public String regisSuccess(String username) {
+                msg = "HTML Mail Code";
+                return msg;
+            }
+
+            public String getMsg() {
+                return msg;
+            }
+
+            public void setMsg(String msg) {
+                this.msg = msg;
+            }
+        }
+
         return false;
 
     }
