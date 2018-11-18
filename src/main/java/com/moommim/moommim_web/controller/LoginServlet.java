d3f0cee89e77047c7c28af19a4b26dcdddd5b093
Unknown <nuntuch13589@gmail.com>
2018-11-18 22:19:37 +0700

    สร้าง view Login

---
+++ src/main/java/com/moommim/moommim_web/controller/LoginServlet.java
@@ -0,0 +1,52 @@
+package com.moommim.moommim_web.controller;
+
+import com.moommim.moommim_web.config.Path;
+import com.moommim.moommim_web.controller.base.BaseController;
+import com.moommim.moommim_web.model.UserAccount;
+import com.moommim.moommim_web.service.base.AuthenticationService;
+import com.moommim.moommim_web.service.base.ExampleService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.Session;
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
+public class LoginServlet extends BaseController {
+
+    @Inject
+    private AuthenticationService authenticationService;
+
+    @Override
+    public void init() throws ServletException {
+        authenticationService.setJpaController(emf, utx);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserAccount userAccountObj = authenticationService.login(email, password);
+
+        if (userAccountObj != null) {
+            HttpServletRequest session = (HttpServletRequest) request.getSession();
+            session.setAttribute("userAccountObj", userAccountObj);
+            sendToPage(Path.INDEX_VIEW, request, response);
+
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}
