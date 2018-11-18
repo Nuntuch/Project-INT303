71030f5177fbd51b88ffb618a30ab0833b636c17
Unknown <nuntuch13589@gmail.com>
2018-11-18 11:27:47 +0700

    สร้าง AuthenticationServiceImplJUnitTest

---
+++ src/test/java/com/moommim/moommim_web/service/AuthenticationServiceImplJUnitTest.java
@@ -0,0 +1,41 @@
+package com.moommim.moommim_web.service;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nuntuch Thongyoo
+ */
+public class AuthenticationServiceImplJUnitTest {
+    
+    public AuthenticationServiceImplJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+   
+}
