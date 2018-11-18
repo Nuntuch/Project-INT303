9f026010c9333f0c9a55914c46d0724c356927a0
Unknown <nuntuch13589@gmail.com>
2018-11-18 12:42:08 +0700

    เขียนลอจิกใน medthod Logout ของ  AuthenticationServiceImpl

--- src/main/java/com/moommim/moommim_web/service/AuthenticationServiceImpl.java
+++ src/main/java/com/moommim/moommim_web/service/AuthenticationServiceImpl.java
@@ -14,50 +14,56 @@ import javax.transaction.UserTransaction;
  *
  * @author Nuntuch Thongyoo
  */
-public class AuthenticationServiceImpl implements AuthenticationService{
+public class AuthenticationServiceImpl implements AuthenticationService {
 
-     @PersistenceUnit(unitName = "com.moommim_moommim_web_war_1.0-SNAPSHOTPU")
+    @PersistenceUnit(unitName = "com.moommim_moommim_web_war_1.0-SNAPSHOTPU")
     EntityManagerFactory emf;
 
     @Resource
     UserTransaction utx;
-    
+
     @Override
     public UserAccount login(String username, String password) {
-       
-        if(username != null && username.trim().length()>0
-                && password != null && password.trim().length()>0
-                ){
-        
-        UserAccountJpaController accountJpaController = new UserAccountJpaController(utx, emf);
-        List <UserAccount> userAccountList = accountJpaController.findUserAccountEntities();
-        
-        for (UserAccount userAccount : userAccountList) {
-            if(userAccount.getEmail().equals(username)){
-                if(userAccount.getPassword().equals(password)){
-                return userAccount;
+
+        if (username != null && username.trim().length() > 0
+                && password != null && password.trim().length() > 0) {
+
+            UserAccountJpaController accountJpaController = new UserAccountJpaController(utx, emf);
+            List<UserAccount> userAccountList = accountJpaController.findUserAccountEntities();
+
+            for (UserAccount userAccount : userAccountList) {
+                if (userAccount.getEmail().equals(username)) {
+                    if (userAccount.getPassword().equals(password)) {
+                        return userAccount;
+                    }
+
                 }
-                
             }
         }
-        }
         return null;
-        
+
     }
 
     @Override
     public boolean logout(HttpSession session) {
+        try {
+            session.invalidate();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+
+@Override
+        public boolean isLogin(HttpSession session) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean isLogin(HttpSession session) {
+        public boolean ForgoutPassword(String username) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public boolean ForgoutPassword(String username) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
