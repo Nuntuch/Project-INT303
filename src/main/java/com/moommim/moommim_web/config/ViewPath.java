75fd33f8b343e1ad1c4a8440ec4ca8fe20302d7b
Nuntuch <nuntuch13589@gmail.com>
2018-11-19 17:39:04 +0700

    แก้ชื่อไฟล์เป็น ViewPath เก็บตำแหน่ง view

--- src/main/java/com/moommim/moommim_web/config/ViewPath.java
+++ src/main/java/com/moommim/moommim_web/config/ViewPath.java
@@ -1,11 +1,15 @@
 package com.moommim.moommim_web.config;
 
-public class Path {
+public class ViewPath {
 
     public static final String BASE_PAGE = "/pages";
-    
+
     public static final String BASE_HOME_PAGE = "/home";
-    
+
+    public static final String BASE_LOGIN_PAGE = "/login";
+
     public static final String INDEX_VIEW = BASE_PAGE + BASE_HOME_PAGE + "/index.jsp";
-    
+
+    public static final String LOGIN_VIEW = BASE_PAGE + BASE_LOGIN_PAGE + "/index.jsp";
+
 }
