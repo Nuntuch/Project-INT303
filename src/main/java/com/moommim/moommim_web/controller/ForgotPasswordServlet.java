6845441dc64f81e51867a394ac15a42694ba6665
Nuntuch <nuntuch13589@gmail.com>
2018-11-22 13:38:03 +0700

    เขียนลอจิกใน ForgotPasswordServlet

--- src/main/java/com/moommim/moommim_web/controller/ForgotPasswordServlet.java
+++ src/main/java/com/moommim/moommim_web/controller/ForgotPasswordServlet.java
@@ -1,31 +1,28 @@
 package com.moommim.moommim_web.controller;
 
+import com.moommim.moommim_web.config.ServletPath;
+import com.moommim.moommim_web.config.ViewPath;
+import com.moommim.moommim_web.controller.base.BaseController;
+import com.moommim.moommim_web.service.AuthenticationServiceImpl;
+import com.moommim.moommim_web.util.Util;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet(name = "ForgotPasswordServlet", urlPatterns = {"/forgotpassword"})
-public class ForgotPasswordServlet extends HttpServlet {
-
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+public class ForgotPasswordServlet extends BaseController {
+    
+    @Inject
+    private AuthenticationServiceImpl authenticationService;
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      
+        
+        sendToPage(ViewPath.FORGOTPASSWORD_VIEW, request, response);
     }
 
     /**
@@ -39,17 +36,15 @@ public class ForgotPasswordServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-     
+        String email = request.getParameter("email");
+        
+        if (Util.isNotEmpty(email)) {
+            
+            authenticationService.forgotPassword(email);
+            
+            sendToPage(ServletPath.LOGIN_SERVLET, request, response);
+            return;
+        }
+        sendToPage(ViewPath.FORGOTPASSWORD_VIEW, request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
