6071dd9f129211e0f51fb52ea615b5e57fcae43f
Nuntuch <nuntuch13589@gmail.com>
2018-11-23 12:17:48 +0700

    เปลี่ยนมา  inject   AuthenticationService authenticationService;

--- src/main/java/com/moommim/moommim_web/controller/LoginServlet.java
+++ src/main/java/com/moommim/moommim_web/controller/LoginServlet.java
@@ -1,41 +1,34 @@
 package com.moommim.moommim_web.controller;
 
 import com.moommim.moommim_web.config.App;
+import com.moommim.moommim_web.config.Key;
 import com.moommim.moommim_web.config.ServletPath;
 import com.moommim.moommim_web.config.ViewPath;
 import com.moommim.moommim_web.controller.base.BaseController;
 import com.moommim.moommim_web.model.UserAccount;
-import com.moommim.moommim_web.service.AuthenticationServiceImpl;
 import com.moommim.moommim_web.service.base.AuthenticationService;
-import com.moommim.moommim_web.service.base.ExampleService;
 import com.moommim.moommim_web.util.Util;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.Session;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends BaseController {
 
     @Inject
-    private AuthenticationServiceImpl authenticationService;
-
-//    @Override
-//    public void init() throws ServletException {
-//        authenticationService.setJpa(emf, utx);
-//    }
-
+    private AuthenticationService authenticationService;
+ 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         sendToPage(ViewPath.LOGIN_VIEW, request, response);
+
     }
 
     @Override
@@ -48,11 +41,11 @@ public class LoginServlet extends BaseController {
         UserAccount userAccount = authenticationService.login(email, password);
 
         if (Util.isNotEmpty(userAccount)) {
-            System.out.println("User is not null");
-            request.getSession().setAttribute("userAccount", userAccount);
+            request.getSession().setAttribute(Key.USER_ACCOUNT_KEY, userAccount);
             sendRedirectToPage(ServletPath.HOME_SERVLET, response);
             return;
         }
+        
         sendToPage(ViewPath.LOGIN_VIEW, request, response);
 
     }
