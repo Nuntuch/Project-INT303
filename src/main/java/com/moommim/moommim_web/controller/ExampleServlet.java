9484b6448875b5101de7f13f81aca3bb642f527b
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-24 23:05:04 +0700

    ส่งไปหน้า index jsp

--- src/main/java/com/moommim/moommim_web/controller/ExampleServlet.java
+++ src/main/java/com/moommim/moommim_web/controller/ExampleServlet.java
@@ -1,5 +1,6 @@
 package com.moommim.moommim_web.controller;
 
+import com.moommim.moommim_web.config.Path;
 import com.moommim.moommim_web.controller.base.BaseController;
 import com.moommim.moommim_web.model.MailMessage;
 import com.moommim.moommim_web.service.base.MailService;
@@ -22,9 +23,11 @@ public class ExampleServlet extends BaseController {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if (mailService.sendMail("nainatjab999@gmail.com", new MailMessage("Hello Form Moommim :D", "<h1>Send Mail Noti</h1>"))) {
-            LOGGER.fine("Send mail is success");
-        }
+//        if (mailService.sendMail("nainatjab999@gmail.com", new MailMessage("Hello Form Moommim :D", "<h1>Send Mail Noti</h1>"))) {
+//            LOGGER.fine("Send mail is success");
+//        }
+
+        sendToPage(Path.INDEX_VIEW, request, response);
     }
 
     @Override
