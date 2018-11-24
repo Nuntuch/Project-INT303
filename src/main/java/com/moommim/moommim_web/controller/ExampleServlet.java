33f19d9aa863d23c57637ed5dc4dd5dd1399ca6e
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-24 15:25:57 +0700

    เพิ่มตัวอย่างการส่งเมล์

--- src/main/java/com/moommim/moommim_web/controller/ExampleServlet.java
+++ src/main/java/com/moommim/moommim_web/controller/ExampleServlet.java
@@ -1,13 +1,11 @@
 package com.moommim.moommim_web.controller;
 
 import com.moommim.moommim_web.controller.base.BaseController;
-import com.moommim.moommim_web.model.Ads;
-import com.moommim.moommim_web.service.base.ExampleService;
-import com.moommim.moommim_web.util.Util;
+import com.moommim.moommim_web.model.MailMessage;
+import com.moommim.moommim_web.service.base.MailService;
 import java.io.IOException;
-import java.util.List;
+import java.util.logging.Logger;
 import javax.inject.Inject;
-import javax.persistence.NoResultException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,26 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ExampleServlet", urlPatterns = {"/example"})
 public class ExampleServlet extends BaseController {
 
+    private static final Logger LOGGER = Logger.getGlobal();
+
     @Inject
-    private ExampleService exampleService;
+    private MailService mailService;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            List<Ads> ads = exampleService.getAllAds();
-            Ads ad = exampleService.getAdsById(2);
-            if (Util.isNotEmpty(ad)) {
-//                System.out.println("Ads Before Update: " + ad.getName());
-//                ad.setName("Lazada.co.th");
-//                ad = exampleService.updateAds(ad);
-//                System.out.println("Ads After Update: " + ad.getName());
-                for (Ads ad1 : ads) {
-                    System.out.println("Ads List: " + ad1.getName());
-                }
-            }
-        } catch (NoResultException ex) {
-            System.out.println("Is No Result");
+        if (mailService.sendMail("nainatjab999@gmail.com", new MailMessage("Hello Form Moommim :D", "<h1>Send Mail Noti</h1>"))) {
+            LOGGER.fine("Send mail is success");
         }
     }
 
