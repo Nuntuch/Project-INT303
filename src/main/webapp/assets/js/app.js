7183306a4cc41feb9a3eb6a33e3310da384c8f95
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-26 01:14:02 +0700

    ย้าย script แสดงปุ่มแชท support ลูกค้าไปยัง app.js

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -1,10 +1,12 @@
 
-// Set Dropdown Trigger
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+
 $(document)
         .ready(function () {
             handleDropDown();
             handleCardDimmer();
             handleRating();
+            showChatSupport();
         });
 
 function handleDropDown() {
@@ -37,4 +39,13 @@ function handleRating() {
     $('.star.rating')
             .rating()
             ;
+}
+
+function showChatSupport() {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5bf9732240105007f3796cbd/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
 }
