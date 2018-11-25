af647288b9dd68bdd1fd5dc2228da017457cddd5
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-25 20:10:49 +0700

    เพิ่มการค้นหาสินค้าโดยระบุสถานะ

--- src/main/java/com/moommim/moommim_web/repository/ProductStockRepository.java
+++ src/main/java/com/moommim/moommim_web/repository/ProductStockRepository.java
@@ -1,22 +1,25 @@
 package com.moommim.moommim_web.repository;
 
-import com.moommim.moommim_web.model.ProductCategory;
 import java.util.List;
 import javax.transaction.Transactional;
+
+import com.moommim.moommim_web.model.ProductStock;
 import org.apache.deltaspike.data.api.AbstractEntityRepository;
 import org.apache.deltaspike.data.api.Repository;
 
 @Transactional
 @Repository
-public abstract class ProductStockRepository extends AbstractEntityRepository<ProductCategory, Integer> {
+public abstract class ProductStockRepository extends AbstractEntityRepository<ProductStock, Integer> {
 
-    public abstract ProductCategory findById(int id);
+    public abstract ProductStock findById(int id);
 
-    public abstract ProductCategory findByName(String name);
+    public abstract ProductStock findByName(String name);
 
-    public abstract ProductCategory findByNameLike(String name);
+    public abstract ProductStock findByNameLike(String name);
 
-    public abstract List<ProductCategory> findByCategoryId(int categoryId);
+    public abstract List<ProductStock> findByCategoryId_id(int categoryId);
+    
+    public abstract List<ProductStock> findByCategoryId_idAndStatusAndIsShow(int categoryId, String status, String isShow);
 
     public abstract int removeById(int id);
 
