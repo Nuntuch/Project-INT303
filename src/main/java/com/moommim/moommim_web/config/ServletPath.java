a6c2ea96c02de29338c3559013ee79ba17721b5b
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-26 02:32:58 +0700

    เพิ่ม path จัดการ product view และ servlet

--- src/main/java/com/moommim/moommim_web/config/ServletPath.java
+++ src/main/java/com/moommim/moommim_web/config/ServletPath.java
@@ -1,13 +1,15 @@
 package com.moommim.moommim_web.config;
 
 public class ServletPath {
-        
+
     public static final String HOME_SERVLET = "";
 
     public static final String LOGIN_SERVLET = "login";
 
     public static final String PRODUCT_BY_CATEGORY_SERVLET = "category";
-    
+
     public static final String PRODUCT_SERVLET = "product";
-    
+
+    public static final String ADMIN_MANAGE_PRODUCT_SERVLET = "admin/product";
+
 }
--- src/main/java/com/moommim/moommim_web/config/ViewPath.java
+++ src/main/java/com/moommim/moommim_web/config/ViewPath.java
@@ -3,13 +3,15 @@ package com.moommim.moommim_web.config;
 public class ViewPath {
 
     public static final String BASE_PAGE = "/pages";
-    
+
     public static final String BASE_HOME_PAGE = "/home";
 
     public static final String BASE_PRODUCT_PAGE = "/product";
-    
+
     public static final String INDEX_VIEW = BASE_PAGE + BASE_HOME_PAGE + "/index.jsp";
 
     public static final String PRODUCT_BY_CATEGORY_VIEW = BASE_PAGE + BASE_PRODUCT_PAGE + "/show_by_category.jsp";
-    
+
+    public static final String ADD_PRODUCT_VIEW = BASE_PAGE + BASE_PRODUCT_PAGE + "/add_product.jsp";
+
 }
