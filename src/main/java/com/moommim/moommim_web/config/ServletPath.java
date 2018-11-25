c070e0b082743553ae43befc240343decbb94bec
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-25 20:08:25 +0700

    เพิ่ม product servlet path

--- src/main/java/com/moommim/moommim_web/config/ServletPath.java
+++ src/main/java/com/moommim/moommim_web/config/ServletPath.java
@@ -5,5 +5,9 @@ public class ServletPath {
     public static final String HOME_SERVLET = "";
 
     public static final String LOGIN_SERVLET = "login";
+
+    public static final String PRODUCT_BY_CATEGORY_SERVLET = "category";
+    
+    public static final String PRODUCT_SERVLET = "product";
     
 }
