56c191ac36144891c8a5b07396fb0b7e4fc0eeda
Nuntuch <nuntuch13589@gmail.com>
2018-11-28 01:24:02 +0700

    เปลี่ยนชื่อเป็นตัวใหญ่

--- src/main/java/com/moommim/moommim_web/config/UserActivateConstant.java
+++ src/main/java/com/moommim/moommim_web/config/UserActivateConstant.java
@@ -1,8 +1,8 @@
 package com.moommim.moommim_web.config;
 
 public class UserActivateConstant {
-    public static final String Not_Active = "not_active";
+    public static final String NOT_ACTIVE = "not_active";
     
-    public static final String Activated = "activated";
+    public static final String ACTIVATED = "activated";
     
 }
