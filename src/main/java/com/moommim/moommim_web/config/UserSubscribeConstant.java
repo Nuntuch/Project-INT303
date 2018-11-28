3a9a6b946e64ab3eb32c2691ddca69c14ccaa1c7
Nuntuch <nuntuch13589@gmail.com>
2018-11-28 22:11:35 +0700

    สร้าง UserSubscribeConstant

---
+++ src/main/java/com/moommim/moommim_web/config/UserSubscribeConstant.java
@@ -0,0 +1,9 @@
+package com.moommim.moommim_web.config;
+
+public class UserSubscribeConstant {
+        
+    public static final String DONT_SUBSCRIBE = "dont_subscribe";
+    
+    public static final String SUBSCRIBE = "subscribed";
+            
+}
