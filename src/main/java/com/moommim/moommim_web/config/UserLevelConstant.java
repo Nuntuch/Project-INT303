502da96f83c0ce4ded281f3e4534c108c2535a67
Nuntuch <nuntuch13589@gmail.com>
2018-11-28 19:08:37 +0700

    สร้าง UserLevelConstant

---
+++ src/main/java/com/moommim/moommim_web/config/UserLevelConstant.java
@@ -0,0 +1,20 @@
+package com.moommim.moommim_web.config;
+
+public class UserLevelConstant {
+        
+    public static final String NORMAL = "user_normal";
+    
+    public static final String BRONZE = "user_bronze";
+    
+    public static final String SILVER = "user_silver";
+    
+    public static final String GOLD = "user_gold";
+  
+    public static final String ADMIN = "user_admin";
+        
+  
+
+        
+    
+        
+}
