e725299978f8d1567fac1e18378f8d782b798cab
Nuntuch <nuntuch13589@gmail.com>
2018-11-28 21:40:45 +0700

    สร้าง UserSexConstant

---
+++ src/main/java/com/moommim/moommim_web/config/UserSexConstant.java
@@ -0,0 +1,16 @@
+package com.moommim.moommim_web.config;
+
+public class UserSexConstant {
+        
+    public static final String MALE = "male";
+    
+    public static final String FEMALE = "female";
+    
+    public static final String OTHER = "other";
+        
+  
+
+        
+    
+        
+}
