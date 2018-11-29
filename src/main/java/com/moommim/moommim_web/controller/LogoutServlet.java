68a25cd6fdef268d69388368a3a582b0382f9a87
Nuntuch <nuntuch13589@gmail.com>
2018-11-29 16:41:15 +0700

    เขียนลอจิก ของ Login เสร็จแล้ว

--- src/main/java/com/moommim/moommim_web/controller/LogoutServlet.java
+++ src/main/java/com/moommim/moommim_web/controller/LogoutServlet.java
@@ -1,19 +1,32 @@
 package com.moommim.moommim_web.controller;
 
+import com.moommim.moommim_web.config.ServletPath;
 import com.moommim.moommim_web.controller.base.BaseController;
+import com.moommim.moommim_web.service.base.AuthenticationService;
+import com.moommim.moommim_web.util.Util;
 import java.io.IOException;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "LogoutServlet", urlPatterns = {"/logout"})
 public class LogoutServlet extends BaseController {
-
+    
+    @Inject
+    AuthenticationService authenticationService;
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        
+        HttpSession session = request.getSession(false);
+        if (Util.isNotEmpty(session)) {
+            authenticationService.logout(session);
+        }
+        sendRedirectToPage(ServletPath.HOME_SERVLET, response);
     }
-
+    
 }
