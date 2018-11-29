0dddba6e70ad1959b95f5411a1c7daaa63ca0206
Nuntuch <nuntuch13589@gmail.com>
2018-11-29 21:41:21 +0700

    เพิ่มเมตตอดสร้าง Bill และ  หา Bill จาก Id

--- src/main/java/com/moommim/moommim_web/service/BillServiceImpl.java
+++ src/main/java/com/moommim/moommim_web/service/BillServiceImpl.java
@@ -4,20 +4,22 @@ import com.moommim.moommim_web.model.Bill;
 import com.moommim.moommim_web.repository.BillRepository;
 import com.moommim.moommim_web.service.base.BillService;
 import com.moommim.moommim_web.util.Util;
+import java.util.List;
 import javax.inject.Inject;
 
-/**
- *
- * @author Nuntuch Thongyoo
- */
 public class BillServiceImpl implements BillService {
 
     @Inject
     BillRepository billRepository;
 
     @Override
-    public boolean AddBillToDB(Bill bill) {
-        return Util.isNotEmpty(billRepository.save(bill));
+    public Bill create(Bill bill) {
+        return billRepository.save(bill);
+    }
+
+    @Override
+    public List<Bill> getBillByUserId(int userId) {
+        return billRepository.findOptionalByUser_Id_id(userId);
     }
 
 }
