915a9dde5c3ac8f0f7a566deb0be8a970b07cf43
Nuntuch <nuntuch13589@gmail.com>
2018-11-29 21:43:34 +0700

    เพิ่ม  findOptionalByUser_Id_id

--- src/main/java/com/moommim/moommim_web/repository/BillRepository.java
+++ src/main/java/com/moommim/moommim_web/repository/BillRepository.java
@@ -1,9 +1,9 @@
 package com.moommim.moommim_web.repository;
 
 import com.moommim.moommim_web.model.Bill;
+import java.util.List;
 import javax.transaction.Transactional;
 
-import com.moommim.moommim_web.model.UserAccount;
 import org.apache.deltaspike.data.api.AbstractEntityRepository;
 import org.apache.deltaspike.data.api.Repository;
 
@@ -11,4 +11,6 @@ import org.apache.deltaspike.data.api.Repository;
 @Repository
 public abstract class BillRepository extends AbstractEntityRepository<Bill, Integer> {
 
+    public abstract List<Bill> findOptionalByUser_Id_id(int userId);
+    
 }
