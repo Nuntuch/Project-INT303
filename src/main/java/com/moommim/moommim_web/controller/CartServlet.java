4ebd1078765896da02db841a4acbbd8742dea486
Nuntuch <nuntuch13589@gmail.com>
2018-11-29 19:26:45 +0700

    ลบ logic ที่ไม่ใช้(เขียนผิด Servlet)

--- src/main/java/com/moommim/moommim_web/controller/CartServlet.java
+++ src/main/java/com/moommim/moommim_web/controller/CartServlet.java
@@ -71,53 +71,9 @@ public class CartServlet extends BaseController {
         sendToPage(ViewPath.SHOW_CART_VIEW, request, response);
     }
 
-    @Inject
-    BillService billService;
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-//                                        <textarea rows="2" name="address" placeholder="ชื่อ ที่อยู่สำหรับจัดส่งสินค้า" required></textarea>
-//                                        <input type="text" name="cardOwner" placeholder="ชื่อผู้ถือบัตร" required>
-//                                        <input type="number" name="cardExpireMonth" maxlength="4" placeholder="MM" required>
-//                                        <input type="number" name="cardExpireYear" maxlength="4" placeholder="YY" required>
-//                                        <input type="number" name="cardNumber" maxlength="16" placeholder="เลขบัตร" required>
-//                                        <input type="number" name="cardCVC" maxlength="3" placeholder="CVC" required>
-//                                
-        String address = request.getParameter("address");
-        String cardOwner = request.getParameter("cardOwner");
-        String cardExpireMonth = request.getParameter("cardExpireMonth");
-        String cardExpireYear = request.getParameter("cardExpireYear");
-        String cardNumber = request.getParameter("cardNumber");
-        String cardCVC = request.getParameter("cardCVC");
-
-        if (Util.isNotEmpty(address)
-                && Util.isNotEmpty(cardOwner)
-                && Util.isNotEmpty(cardExpireMonth)
-                && Util.isNotEmpty(cardExpireYear)
-                && Util.isNotEmpty(cardNumber)
-                && Util.isNotEmpty(cardCVC)) {
-            
-           HttpSession session = request.getSession(false);
-           
-            CartItem cartItem = (CartItem) session.getAttribute(Key.CART_KEY);
-            UserAccount userAccount = (UserAccount) session.getAttribute(Key.USER_ACCOUNT_KEY);
-            
-            Bill bill = new Bill();
-            bill.setAddress(address);
-            bill.setCreateAt(new Date());
-            bill.setTotalPrice(cartItem.getTotalPrice());
-            bill.setUserId(userAccount);
-            
-       
-             
-            billService.AddBillToDB(bill);
-            
-            sendToPage(ServletPath.HOME_SERVLET, request, response);
-
-        }
-        sendToPage(ViewPath.CHECKOUT_VIEW, request, response);
 
     }
 
