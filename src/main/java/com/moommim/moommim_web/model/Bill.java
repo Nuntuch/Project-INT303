acffeab0659bcd91ef1b28362175120fe1c7b252
Nuntuch <nuntuch13589@gmail.com>
2018-11-29 17:07:55 +0700

    สร้าง model ใหม่

---
+++ src/main/java/com/moommim/moommim_web/model/Bill.java
@@ -0,0 +1,141 @@
+package com.moommim.moommim_web.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Nuntuch Thongyoo
+ */
+@Entity
+@Table(name = "BILL")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Bill.findAll", query = "SELECT b FROM Bill b")
+    , @NamedQuery(name = "Bill.findById", query = "SELECT b FROM Bill b WHERE b.id = :id")
+    , @NamedQuery(name = "Bill.findByAddress", query = "SELECT b FROM Bill b WHERE b.address = :address")
+    , @NamedQuery(name = "Bill.findByCreateAt", query = "SELECT b FROM Bill b WHERE b.createAt = :createAt")
+    , @NamedQuery(name = "Bill.findByTotalPrice", query = "SELECT b FROM Bill b WHERE b.totalPrice = :totalPrice")})
+public class Bill implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 200)
+    @Column(name = "ADDRESS")
+    private String address;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CREATE_AT")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "TOTAL_PRICE")
+    private String totalPrice;
+    @JoinColumn(name = "USER_ID", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private UserAccount userId;
+
+    public Bill() {
+    }
+
+    public Bill(Integer id) {
+        this.id = id;
+    }
+
+    public Bill(Integer id, String address, Date createAt, String totalPrice) {
+        this.id = id;
+        this.address = address;
+        this.createAt = createAt;
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(String totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public UserAccount getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UserAccount userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Bill)) {
+            return false;
+        }
+        Bill other = (Bill) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.moommim.moommim_web.model.Bill[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/com/moommim/moommim_web/model/ProductSale.java
@@ -0,0 +1,168 @@
+package com.moommim.moommim_web.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Nuntuch Thongyoo
+ */
+@Entity
+@Table(name = "PRODUCT_SALE")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ProductSale.findAll", query = "SELECT p FROM ProductSale p")
+    , @NamedQuery(name = "ProductSale.findById", query = "SELECT p FROM ProductSale p WHERE p.id = :id")
+    , @NamedQuery(name = "ProductSale.findByQuantity", query = "SELECT p FROM ProductSale p WHERE p.quantity = :quantity")
+    , @NamedQuery(name = "ProductSale.findByAmount", query = "SELECT p FROM ProductSale p WHERE p.amount = :amount")
+    , @NamedQuery(name = "ProductSale.findByBillId", query = "SELECT p FROM ProductSale p WHERE p.billId = :billId")
+    , @NamedQuery(name = "ProductSale.findByCreateAt", query = "SELECT p FROM ProductSale p WHERE p.createAt = :createAt")
+    , @NamedQuery(name = "ProductSale.findByPricePerUnit", query = "SELECT p FROM ProductSale p WHERE p.pricePerUnit = :pricePerUnit")})
+public class ProductSale implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "QUANTITY")
+    private int quantity;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "AMOUNT")
+    private BigDecimal amount;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "BILL_ID")
+    private int billId;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CREATE_AT")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PRICE_PER_UNIT")
+    private BigDecimal pricePerUnit;
+    @JoinColumn(name = "PRODUCT_ID", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private ProductStock productId;
+
+    public ProductSale() {
+    }
+
+    public ProductSale(Integer id) {
+        this.id = id;
+    }
+
+    public ProductSale(Integer id, int quantity, BigDecimal amount, int billId, Date createAt, BigDecimal pricePerUnit) {
+        this.id = id;
+        this.quantity = quantity;
+        this.amount = amount;
+        this.billId = billId;
+        this.createAt = createAt;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public int getBillId() {
+        return billId;
+    }
+
+    public void setBillId(int billId) {
+        this.billId = billId;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public BigDecimal getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public void setPricePerUnit(BigDecimal pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public ProductStock getProductId() {
+        return productId;
+    }
+
+    public void setProductId(ProductStock productId) {
+        this.productId = productId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ProductSale)) {
+            return false;
+        }
+        ProductSale other = (ProductSale) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.moommim.moommim_web.model.ProductSale[ id=" + id + " ]";
+    }
+    
+}
