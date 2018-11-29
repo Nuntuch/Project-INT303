c46d2ba03d235200d319be1bf035641a0299fe94
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-11-29 09:57:55 +0700

    เพิ่มการสมัครสมาชิก

--- src/main/java/com/moommim/moommim_web/controller/RegisterServlet.java
+++ src/main/java/com/moommim/moommim_web/controller/RegisterServlet.java
@@ -1,9 +1,12 @@
 package com.moommim.moommim_web.controller;
 
+import com.moommim.moommim_web.config.Key;
 import com.moommim.moommim_web.config.ServletPath;
 import com.moommim.moommim_web.config.ViewPath;
 import com.moommim.moommim_web.controller.base.BaseController;
-import com.moommim.moommim_web.service.base.ProductService;
+import com.moommim.moommim_web.model.UserAccount;
+import com.moommim.moommim_web.service.base.UserService;
+import com.moommim.moommim_web.util.Util;
 import java.io.IOException;
 import java.util.logging.Logger;
 import javax.inject.Inject;
@@ -18,11 +21,38 @@ public class RegisterServlet extends BaseController {
     private static final Logger LOGGER = Logger.getLogger(RegisterServlet.class.getName());
 
     @Inject
-    private ProductService productService;
+    private UserService userService;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        sendToPage(ViewPath.REGISTER_VIEW, request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String mobile = request.getParameter("mobile");
+
+        if (Util.isNotEmpty(email)) {
+            if (Util.isNotEmpty(userService.getUserByEmail(email))) {
+                request.setAttribute("emailStatus", "อีเมล์นี้มีผู้ใช้งานแล้ว");
+            }
+            if (Util.isNotEmpty(password)
+                    && Util.isNotEmpty(firstName)
+                    && Util.isNotEmpty(lastName)
+                    && Util.isNotEmpty(mobile)) {
+                UserAccount newUser = new UserAccount(email, password, firstName, lastName, mobile);
+                if (userService.create(newUser)) {
+                    request.getSession(true).setAttribute(Key.USER_ACCOUNT_KEY, newUser);
+                    sendRedirectToPage(ServletPath.HOME_SERVLET, response);
+                    return;
+                }
+            }
+        }
         sendToPage(ViewPath.REGISTER_VIEW, request, response);
     }
 
